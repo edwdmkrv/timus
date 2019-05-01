994340c64ac3f63bc87e8ed584a773a0af644134
edwdmkrv <46138366+edwdmkrv@users.noreply.github.com>
2019-05-01 20:38:45 +0300

    1306/test.cpp: remove non-portable debug code

--- 1306/test.cpp
+++ 1306/test.cpp
@@ -40,11 +40,6 @@ TEST(MainGroup, Test_1306) {
 		average << static_cast<long double>(n3 + n4) / 2;
 		result << r;
 
-		FILE *const wr{fopen("/arc/Courses/OTUS/C++/timus/AAA", "w")};
-		fprintf(wr, "result: '%s'\n", result.str().c_str());
-		fprintf(wr, "average: '%s'\n", average.str().c_str());
-//std::cerr << "result: '" << result.str() << "'" << std:endl;
-//std::cerr << "average: '" << average.str() << "'" << std:endl;
 		EXPECT_EQ(result.str(), average.str());
 	} catch (...) {
 		ASSERT_TRUE(false);
