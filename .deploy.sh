28c83c3ef3d76317073354230a95a348589bd3af
edwdmkrv <46138366+edwdmkrv@users.noreply.github.com>
2019-05-01 21:24:27 +0300

    .deploy.sh: add error-persistent logic

--- .deploy.sh
+++ .deploy.sh
@@ -10,7 +10,30 @@ version="0.0.$1"
 user='edwdmkrv'
 params=';deb_distribution=xenial;deb_component=main;deb_architecture=amd64;publish=1'
 
-for component in 1000 1001 1073 1196 1197 1264 1293 1306 1409 1496 1545 1639 1785 1787 1820 1837 1864 1877 1880 2001 2012 2066; do
-	package="${name}-${component}_${version}_amd64.deb"
-	curl -T "$package" -u "${user}:$2" "$baseurl/$user/$project/$name/$version/pool/main/${name::1}/$name/$package$params"
+attempt='0'
+attempts='9'
+components='1000 1001 1073 1196 1197 1264 1293 1306 1409 1496 1545 1639 1785 1787 1820 1837 1864 1877 1880 2001 2012 2066'
+failed=''
+
+while test "$attempt" -lt "$attempts"; do
+	for component in $components; do
+		package="${name}-${component}_${version}_amd64.deb"
+
+		curl -T "$package" -u "${user}:$2" "$baseurl/$user/$project/$name/$version/pool/main/${name::1}/$name/$package$params"
+
+		if test $? -ne 0; then
+			failed="$failed $component"
+		fi
+	done
+
+	attempt="`expr "$attempt" + 1`"
+
+	if test x"$failed" != x"$components"; then
+		attempt='0'
+		components="$failed"
+	fi
+
+	if test -z "$components"; then
+		break
+	fi
 done
